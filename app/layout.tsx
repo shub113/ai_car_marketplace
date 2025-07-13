bb61a3f4b49986c198e979d63285eb2d0613d6b1
Shubham Thakur <shub_mac@Shubhams-MacBook-Pro.local>
2025-07-13 15:38:12 +0530

    added linkedIN profile

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {
   ClerkProvider,
 } from '@clerk/nextjs'
 import { Toaster } from "sonner";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,9 +33,11 @@ export default function RootLayout({
           <Toaster richColors />
 
           {/* Footer */}
-          <footer className="bg-blue-50 py-12" >
-            <div className="container mx-auto px-4 text-center text-gray-600" >
-              Footer
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with 💗 by  <Link className="text-blue-600 cursor-pointer underline" href="https://www.linkedin.com/in/shubham-thakur311/" >
+                SHUBHAM THAKUR
+              </Link></p>
             </div>
           </footer>
         </body>
